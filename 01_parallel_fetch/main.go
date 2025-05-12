1f1bea9cc99508746260c87dc20a62a8bdfcffd6
Jont Olof Lyttkens <jontolof@mac.com>
2025-05-13 01:51:29 +0200

    Solve 01_parallel_fetch

--- 01_parallel_fetch/main.go
+++ 01_parallel_fetch/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+func main() {
+	fmt.Println("Start")
+	var wg sync.WaitGroup
+	urls := []string{
+		"http://www.aftonbladet.se",
+		"http://www.apple.com",
+		"http://www.svd.se",
+	}
+	for _, url := range urls {
+		wg.Add(1)
+		go fetchURL(url, &wg)
+	}
+	wg.Wait()
+}
+
+func fetchURL(url string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+	fmt.Println("Status Code:", resp.StatusCode)
+}
